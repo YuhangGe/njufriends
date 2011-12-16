c67e0d423759a930ce52e7111bbd520eee0485d5
Yuhang Ge <abeyuhang@gmail.com>
2011-12-16 13:16:42 +0800

    daisy

--- activity.php
+++ activity.php
@@ -14,7 +14,7 @@
         <title>Hello</title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
         <meta property="xn:app_id" content="172440" />
-        <link style="text/css" rel="stylesheet" href="style2.css" />
+        <link style="text/css" rel="stylesheet" href="css/style2.css" />
     </head>
     <body>
 
